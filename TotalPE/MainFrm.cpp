b7e79e1a1f2f9ad965529ee9b17252364ac54bd0
Pavel Yosifovich <zodiacon@live.com>
2022-04-16 18:32:36 +0300

    Add project files.

---
+++ TotalPE/MainFrm.cpp
@@ -0,0 +1,80 @@
+// MainFrm.cpp : implmentation of the CMainFrame class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#include "pch.h"
+#include "resource.h"
+#include "MainFrm.h"
+#include "View.h"
+#include "AboutDlg.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "../LIEF/Liefd.lib")
+#else
+#pragma comment(lib, "../LIEF/Lief.lib")
+#endif
+
+BOOL CMainFrame::PreTranslateMessage(MSG* pMsg) {
+	if (CFrameWindowImpl<CMainFrame>::PreTranslateMessage(pMsg))
+		return TRUE;
+
+	return m_view.PreTranslateMessage(pMsg);
+}
+
+BOOL CMainFrame::OnIdle() {
+	return FALSE;
+}
+
+LRESULT CMainFrame::OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/) {
+	auto pe = LIEF::PE::Parser::parse("c:\\windows\\regedit.exe");
+	for (auto& e : pe->get_export().entries()) {
+	}
+	CreateSimpleStatusBar();
+
+	m_hWndClient = m_view.Create(m_hWnd, rcDefault, nullptr, WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS | WS_CLIPCHILDREN, WS_EX_CLIENTEDGE);
+	UISetCheck(ID_VIEW_STATUS_BAR, 1);
+
+	// register object for message filtering and idle updates
+	CMessageLoop* pLoop = _Module.GetMessageLoop();
+	ATLASSERT(pLoop != nullptr);
+	pLoop->AddMessageFilter(this);
+	pLoop->AddIdleHandler(this);
+
+	return 0;
+}
+
+LRESULT CMainFrame::OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled) {
+	// unregister message filtering and idle updates
+	CMessageLoop* pLoop = _Module.GetMessageLoop();
+	ATLASSERT(pLoop != nullptr);
+	pLoop->RemoveMessageFilter(this);
+	pLoop->RemoveIdleHandler(this);
+
+	bHandled = FALSE;
+	return 1;
+}
+
+LRESULT CMainFrame::OnFileExit(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/) {
+	PostMessage(WM_CLOSE);
+	return 0;
+}
+
+LRESULT CMainFrame::OnFileNew(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/) {
+	// TODO: add code to initialize document
+
+	return 0;
+}
+
+LRESULT CMainFrame::OnViewStatusBar(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/) {
+	BOOL bVisible = !::IsWindowVisible(m_hWndStatusBar);
+	::ShowWindow(m_hWndStatusBar, bVisible ? SW_SHOWNOACTIVATE : SW_HIDE);
+	UISetCheck(ID_VIEW_STATUS_BAR, bVisible);
+	UpdateLayout();
+	return 0;
+}
+
+LRESULT CMainFrame::OnAppAbout(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/) {
+	CAboutDlg dlg;
+	dlg.DoModal();
+	return 0;
+}
